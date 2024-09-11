2fb9f39db23b40a81933df6fcbee7052ebcdf800
Kalib Brayer <kalib.brayer@bytecode.io>
2024-09-11 12:30:04 -0700

    Docs: create load_examples.sh file
    Adding a bash file to simplify the example loading experience.

---
+++ explore-assistant-examples/load_examples.sh
@@ -0,0 +1,12 @@
+PROJECT_ID=”PROJECT_ID”
+DATASET_ID=”DATASET_ID”
+EXPLORE_ID=”PROJECT:EXPLORE_ID”
+TABLE_ID=”explore_assistant_examples”
+JSON_FILE=”examples.json”
+
+python load_examples.py \
+--project_id $PROJECT_ID \
+--dataset_id $DATASET_ID \
+--explore_id $EXPLORE_ID \
+--table_id $TABLE_ID \
+--json_file $JSON_FILE

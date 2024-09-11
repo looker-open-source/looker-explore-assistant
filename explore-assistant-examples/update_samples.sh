af6f36262ed889d7c4d9ffb9711dabe63eb5bca2
Kalib Brayer <kalib.brayer@bytecode.io>
2024-09-11 12:36:22 -0700

    Docs: create update_samples.sh file
    Added bash files to the explore-assistant-examples directory to make loading and updating examples easier.

---
+++ explore-assistant-examples/update_samples.sh
@@ -0,0 +1,13 @@
+PROJECT_ID=”PROJECT_ID”
+DATASET_ID=”DATASET_ID”
+EXPLORE_ID=”PROJECT:EXPLORE_ID”
+TABLE_ID=”explore_assistant_samples”
+JSON_FILE=”samples.json”
+
+python load_examples.py \
+--project_id $PROJECT_ID \
+--dataset_id $DATASET_ID \
+--explore_id $EXPLORE_ID \
+--table_id $TABLE_ID \
+--column_name samples \
+--json_file $JSON_FILE

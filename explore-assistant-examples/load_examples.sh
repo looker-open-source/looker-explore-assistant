140440b242da57ebdcdd6a800357743104a79719
Kalib Brayer <kalib.brayer@bytecode.io>
2024-09-12 15:53:39 -0700

    Docs: Update load_examples.sh
    Adding shebang to each file and file description and model to explore_id reference

--- explore-assistant-examples/load_examples.sh
+++ explore-assistant-examples/load_examples.sh
@@ -1,6 +1,10 @@
+##This script will upload examples to a selected bigquery dataset
+
+#!/bin/sh 
+
 PROJECT_ID=”PROJECT_ID”
 DATASET_ID=”DATASET_ID”
-EXPLORE_ID=”PROJECT:EXPLORE_ID”
+EXPLORE_ID=”MODEL:EXPLORE_ID”
 TABLE_ID=”explore_assistant_examples”
 JSON_FILE=”examples.json”
 

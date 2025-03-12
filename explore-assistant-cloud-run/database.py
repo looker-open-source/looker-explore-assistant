a8fb91011a5198fc9d330f680688c9bd93ab798a
luutuankiet <luutuankiet.ftu2@gmail.com>
2025-03-12 10:08:41 +0700

    init code - dev config for vs code debugger attach - update database py to encode password : some passwords got @ character which needs escape.
    (cherry picked from commit c21fb989db13f8e4899a239f95a08efbf0f3d758)

--- explore-assistant-cloud-run/database.py
+++ explore-assistant-cloud-run/database.py
@@ -1,6 +1,7 @@
 from sqlmodel import create_engine, Session
 import os
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
 load_dotenv()
 
@@ -9,7 +10,10 @@ CLOUD_SQL_USER = os.getenv("CLOUD_SQL_USER")
 CLOUD_SQL_PASSWORD = os.getenv("CLOUD_SQL_PASSWORD")
 CLOUD_SQL_DATABASE = os.getenv("CLOUD_SQL_DATABASE")
 
-DATABASE_URL = f"mysql+pymysql://{CLOUD_SQL_USER}:{CLOUD_SQL_PASSWORD}@{CLOUD_SQL_HOST}/{CLOUD_SQL_DATABASE}"
+ENCODED_PASSWORD = quote_plus(CLOUD_SQL_PASSWORD)  # Encodes special characters
+
+
+DATABASE_URL = f"mysql+pymysql://{CLOUD_SQL_USER}:{ENCODED_PASSWORD}@{CLOUD_SQL_HOST}/{CLOUD_SQL_DATABASE}"
 
 engine = create_engine(DATABASE_URL, echo=False)
 

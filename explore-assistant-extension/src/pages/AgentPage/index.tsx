76c67a2ba6065308c230ce760271a8f090386ebe
colin-roy-ehri <colin.roy.ehri@bytecode.io>
2025-07-10 12:27:23 -0600

    bypass initial checks

--- explore-assistant-extension/src/pages/AgentPage/index.tsx
+++ explore-assistant-extension/src/pages/AgentPage/index.tsx
@@ -287,26 +287,27 @@ const AgentPage = () => {
   }
 
   const isAgentReady = isBigQueryMetadataLoaded && isSemanticModelLoaded
-
   if (!isAgentReady) {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="flex flex-col space-y-4 mx-auto max-w-2xl p-4">
-          <h1 className="text-5xl font-bold">
-            <span className="bg-clip-text text-transparent  bg-gradient-to-r from-pink-500 to-violet-500">
-              Hello.
-            </span>
-          </h1>
-          <h1 className="text-3xl text-gray-400">
-            Getting everything ready...
-          </h1>
-          <div className="max-w-2xl text-blue-300">
-            <LinearProgress color="inherit" />
-          </div>
-        </div>
-      </div>
-    )
-  }
+    console.log('AgentPage - isBigQueryMetadataLoaded:', isBigQueryMetadataLoaded, ' is semanticModelLoaded:', isSemanticModelLoaded)
+      }
+    //       return (
+    //   <div className="flex justify-center items-center h-screen">
+    //     <div className="flex flex-col space-y-4 mx-auto max-w-2xl p-4">
+    //       <h1 className="text-5xl font-bold">
+    //         <span className="bg-clip-text text-transparent  bg-gradient-to-r from-pink-500 to-violet-500">
+    //           Hello.
+    //         </span>
+    //       </h1>
+    //       <h1 className="text-3xl text-gray-400">
+    //         Getting everything ready...
+    //       </h1>
+    //       <div className="max-w-2xl text-blue-300">
+    //         <LinearProgress color="inherit" />
+    //       </div>
+    //     </div>
+    //   </div>
+    // )
+    // }
 
   return (
     <div className="relative page-container flex h-screen">
